ca92a92ea96b0f59304a7ce4f24f6d0c90a89351
eapenzacharias <eapenzac@gmail.com>
2022-03-28 19:41:15 +0200

    Added model for entity

--- db/migrate/20220328171554_create_groups.rb
+++ db/migrate/20220328171554_create_groups.rb
@@ -1,8 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
-    create_table :groups do |t|
+    create_table :categories do |t|
       t.string :name
-      t.belongs_to :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
---
+++ db/migrate/20220328173348_create_entities.rb
@@ -0,0 +1,11 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :invoices do |t|
+      t.string :name
+      t.decimal :amount
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
