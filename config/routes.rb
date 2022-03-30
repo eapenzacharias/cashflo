4d7b204dfc19eb46720b6a3c9ed1ae85c9cc23bd
eapenzacharias <eapenzac@gmail.com>
2022-03-31 00:49:26 +0200

    Setup transaction controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,11 @@ Rails.application.routes.draw do
   get 'invoices/index'
   get 'invoices/new'
   get 'invoices/show'
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :categories, only: [:index, :show, :destroy, :new, :create]
+  resources :transactions, only: [:show, :destroy, :new, :create]
 
   # Defines the root path route ("/")
   root "categories#index"
