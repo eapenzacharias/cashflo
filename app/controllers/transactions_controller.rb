c84d032da14f13a3732d45478735dc236feaeb74
eapenzacharias <eapenzac@gmail.com>
2022-03-31 22:29:14 +0200

    Added Transaction#new

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,10 @@
 class TransactionsController < ApplicationController
   def index; end
 
-  def new; end
+  def new
+    @invoice = Invoice.new
+    @categories = current_user.categories
+  end
 
   def show; end
 end
