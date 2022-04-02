402f27a305cee5caaa658a13328b4a0ba7e2aa55
eapenzacharias <eapenzac@gmail.com>
2022-04-02 20:41:35 +0200

    Updated devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@
 
 Rails.application.routes.draw do
   get 'static/landing'
-  devise_for :users
+  devise_for :users,
+    path: '',
+    path_names: {
+      sign_in: 'signin',
+      sign_out: 'signout',
+      password: 'password',
+      registration: 'register',
+      sign_up: 'signup'
+    }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :categories, only: [:index, :show, :destroy, :new, :create]
