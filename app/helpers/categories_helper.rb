93172134177bf0bd363499497165eb9da21a5c29
eapenzacharias <eapenzac@gmail.com>
2022-04-04 22:02:40 +0200

    Linter fixes

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,7 +1,8 @@
 module CategoriesHelper
   def category_sum(category)
     total_amount = category.invoices.sum(:amount)
-    return "$ 0.00" if total_amount.zero?
+    return '$ 0.00' if total_amount.zero?
+
     "$#{total_amount}"
   end
 end
