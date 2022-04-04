b483e3c94f319a3789059652f39f04c84ba379ea
eapenzacharias <eapenzac@gmail.com>
2022-04-05 01:11:25 +0200

    Fixed styling for data panel

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,13 +1,13 @@
 module CategoriesHelper
-  def category_sum(category)
-    total_amount = category.invoices.sum(:amount)
+  def category_sum(invoices)
+    total_amount = invoices.sum(:amount)
     return '$ 0.00' if total_amount.zero?
 
     "$#{total_amount}"
   end
 
-  def last_month_sum(category)
-    invoices = category.invoices.where('cutdate BETWEEN ? AND ?', Date.current, 30.days.from_now)
+  def last_month_sum(invoices)
+    invoices = invoices.where('created_at BETWEEN ? AND ?', 30.days.ago, Time.now)
     total_amount = invoices.sum(:amount)
     return '$ 0.00' if total_amount.zero?
 
