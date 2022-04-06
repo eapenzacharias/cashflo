bfe3304df2ee843dc7c04b28f9770dc445d0dcb0
eapenzacharias <eapenzac@gmail.com>
2022-04-07 00:29:53 +0200

    Validated Icon presence

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :user }
   has_one_attached :icon
+  validates :icon, presence: true
+
   belongs_to :user
   has_many :invoices
 end
