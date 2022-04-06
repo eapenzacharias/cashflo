8581680db3bbe42391de83ae0e99ed0c5ecea507
eapenzacharias <eapenzac@gmail.com>
2022-04-06 18:37:39 +0200

    Added category uniqueness for each user

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user }
   has_one_attached :icon
   belongs_to :user
   has_many :invoices
