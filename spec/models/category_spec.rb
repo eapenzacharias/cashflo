ffb71a6c8fc0aa092f04bb7635f604ecacc1d248
eapenzacharias <eapenzac@gmail.com>
2022-04-13 23:38:41 +0200

    Updated model tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,26 +4,25 @@ require 'database_cleaner/active_record'
 DatabaseCleaner.strategy = :truncation
 
 RSpec.describe Category, type: :model do
-  user = User.create(id: 1, name: 'User', email: 'user1@test.com', password: 'test1234',
-                     password_confirmation: 'test1234')
-
   before do
     DatabaseCleaner.clean
+    User.create(id: 1, name: 'User', email: 'user1@test.com', password: 'test1234',
+      password_confirmation: 'test1234')
   end
 
   it 'should have a name' do
-    category = Category.new(name: nil, user: user, icon: fixture_file_upload(file_fixture('foods.png')))
+    category = Category.new(name: nil, user_id: 1, icon: fixture_file_upload(file_fixture('foods.png')))
     expect(category).to_not be_valid
   end
 
   it 'should have a unique name' do
-    Category.create(name: 'Foods', user: user, icon: fixture_file_upload(file_fixture('foods.png')))
-    category = Category.new(name: 'Foods', user: user, icon: fixture_file_upload(file_fixture('foods.png')))
+    Category.create(name: 'Foods', user_id: 1, icon: fixture_file_upload(file_fixture('foods.png')))
+    category = Category.new(name: 'Foods', user_id: 1, icon: fixture_file_upload(file_fixture('foods.png')))
     expect(category).to_not be_valid
   end
 
   it 'should have an icon' do
-    category = Category.new(name: 'Foods', user: user)
+    category = Category.new(name: 'Foods', user_id: 1)
     expect(category).to_not be_valid
   end
 end
